c0d4fb8a945ec70dc95d7bebeae2dc80bcbc4915
ikeponias <sephiroth929@gmail.com>
2018-08-18 23:35:08 +0900

    (feat)childTodo

--- src/main/java/com/ikeda/todolist/Controllers/TodoController.java
+++ src/main/java/com/ikeda/todolist/Controllers/TodoController.java
@@ -1,6 +1,7 @@
 package com.ikeda.todolist.Controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -37,21 +38,36 @@ public class TodoController {
 	public String show(Model model, @PathVariable("id") Long id) {
 		Todo todo = todoService.find(id).get();
 		model.addAttribute("todo", todo);
-		model.addAttribute("link_to_new", "/todos/new");
+		model.addAttribute("link_to_child_new", "/todos/new");
 
 		return "todos/show";
 	}
 
-	@RequestMapping(value = "/new", method = RequestMethod.GET)
-	public String newForm(Model model) {
-		model.addAttribute("todoForm", new Todo());
+	@RequestMapping(value = { "/new", "/new/{parent_id}" }, method = RequestMethod.GET)
+	public String newTodo(Model model, @PathVariable(name = "parent_id", required = false) Optional<Long> parent_id) {
+		Todo todo = new Todo();
+
+		model.addAttribute("todoForm", todo);
+		if (parent_id.isPresent()) {
+			model.addAttribute("link_to_create", "/todos/create" + "/" + parent_id.get().toString());
+		} else {
+			model.addAttribute("link_to_create", "/todos/create");
+		}
 
 		return "todos/new";
 	}
 
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public String create(@ModelAttribute Todo todoForm) {
+	@RequestMapping(value = { "/create", "/create/{parent_id}" }, method = RequestMethod.POST)
+	public String create(@ModelAttribute Todo todoForm,
+			@PathVariable(name = "parent_id", required = false) Optional<Long> parent_id) {
+
+		if (parent_id.isPresent()) {
+			Todo parentTodo = todoService.find(parent_id.get()).get();
+			todoForm.setParent(parentTodo);
+		}
+
 		todoService.save(todoForm);
+
 		return "redirect:/todos";
 	}
 
@@ -60,12 +76,18 @@ public class TodoController {
 		Todo todo = todoService.find(id).get();
 		todo.switchDone();
 		todoService.save(todo);
+
 		return "redirect:/todos";
 	}
 
 	@RequestMapping(value = "/destroy/{id}", method = RequestMethod.DELETE)
 	public String destroy(@PathVariable("id") Long id) {
+		List<Todo> childrenTodo = todoService.find(id).get().getChildren();
+		if (childrenTodo != null) {
+			childrenTodo.forEach(child -> todoService.delete(child.getId()));
+		}
 		todoService.delete(id);
+
 		return "redirect:/todos";
 	}
 }
