47fa676f488955f9e7fd6bdecff839a2994a6596
ikeponias <sephiroth929@gmail.com>
2018-08-19 12:25:13 +0900

    URL切り出し

---
+++ src/main/java/com/ikeda/todolist/Constants/RoutingURL.java
@@ -0,0 +1,18 @@
+package com.ikeda.todolist.Constants;
+
+public class RoutingURL {
+	public static final String TODOS = "/todos";
+	public static final String INDEX = "";
+	public static final String SHOW = "/show";
+	public static final String NEW = "/new";
+	public static final String CREATE = "/create";
+	public static final String UPDATE = "/update";
+	public static final String DESTROY = "/destroy";
+
+	public static final String TODOS_INDEX = TODOS + INDEX;
+	public static final String TODOS_SHOW = TODOS + SHOW;
+	public static final String TODOS_NEW = TODOS + NEW;
+	public static final String TODOS_CREATE = TODOS + CREATE;
+	public static final String TODOS_UPDATE = TODOS + UPDATE;
+	public static final String TODOS_DESTROY = TODOS + DESTROY;
+}
--- src/main/java/com/ikeda/todolist/Models/Todo.java
+++ src/main/java/com/ikeda/todolist/Models/Todo.java
@@ -11,14 +11,12 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "todos")
 public class Todo {
 	@Id
